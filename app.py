300d4f9ffd8655fe7595354444b075a6f8984d2d
Gordon Byers <gordonbyers@gmail.com>
2021-02-16 19:39:49 +0000

    Added check for existing files

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os.path
+from os import path
 from pytube import YouTube
 from pytube import Playlist
 
@@ -5,17 +7,18 @@ from pytube import Playlist
 print('PyTube playlist download')
 
 playlist = Playlist("https://www.youtube.com/playlist?list=PL_wl2s89QANvoYBull1jx7pqNaiC6n8xl")
+localFolder = r"C:\Users\gobyers\OneDrive\DJ"
 
 #Output the song count.  If this is 0, then perhaps your YT playlist is private.
 print('Songs in playlist : ', len(playlist))
 
-#List the songs in the playlist.
-for url in playlist:
-    print(url)
-
 #Download the songs locally as MP4, to wherever this script is run from
 for url in playlist:
-    YouTube(url).streams.filter(only_audio=True).first().download()
+    print('Getting metadata for',url)
+    ytStream=YouTube(url).streams.filter(only_audio=True).first()
+    if not path.exists(localFolder + '\\' + ytStream.default_filename):
+        print('--Downloading',ytStream.default_filename)
+        ytStream.download(localFolder)
 
 #TODO: Update the RekordBox XML database
 #https://github.com/Gordonby/rekordbox-scripts
