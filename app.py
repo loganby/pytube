bd4f7f57a8bdf77659e02bf949df379bf57000ba
Gordon Byers <gordonbyers@gmail.com>
2021-04-14 00:18:05 +0100

    Added mp3 conversion with ffmpeg

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os.path
+import subprocess
 from os import path
 from pytube import YouTube
 from pytube import Playlist
@@ -12,13 +13,32 @@ localFolder = r"C:\Users\gobyers\OneDrive\DJ"
 #Output the song count.  If this is 0, then perhaps your YT playlist is private.
 print('Songs in playlist : ', len(playlist))
 
-#Download the songs locally as MP4, to wherever this script is run from
+#Iterate through each item in the playlist
 for url in playlist:
     print('Getting metadata for',url)
+
+    #Download the songs locally as MP4, to wherever this script is run from
     ytStream=YouTube(url).streams.filter(only_audio=True).first()
-    if not path.exists(localFolder + '\\' + ytStream.default_filename):
-        print('--Downloading',ytStream.default_filename)
+    streampath=os.path.join(localFolder, ytStream.default_filename)
+    if not path.exists(streampath):
+        print('--Downloading: ',ytStream.default_filename)
         ytStream.download(localFolder)
+    else:
+        print('--Skipping: ',ytStream.default_filename)
+
+    #Convert MP4 to MP3
+    ytStreamNameMp3=ytStream.default_filename.replace("mp4","mp3")
+    newFilePath=os.path.join(localFolder , 'mp3' , ytStreamNameMp3)
+    if not path.exists(newFilePath):
+        print('--Adding mp3 copy: ',ytStreamNameMp3)
+        subprocess.run([
+            'ffmpeg',
+            '-n',
+            '-i', os.path.join(localFolder, ytStream.default_filename),
+            newFilePath
+        ])
+    else:
+        print('--Skipping mp3 copy: ',ytStreamNameMp3)
 
 #TODO: Update the RekordBox XML database
 #https://github.com/Gordonby/rekordbox-scripts
